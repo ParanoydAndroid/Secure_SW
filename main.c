307a78a9fd9ff8ef69353602b7e9ff4d7c9b0811
brandon <Bkamaka@gmail.com>
2018-11-03 12:38:44 -0400

    Added initializer and modified input.

--- main.c
+++ main.c
@@ -2,31 +2,32 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 
-char* filePath;
-char* paths[2];
-
-
-int getMode( char* string );
-
-int validateMode( char* input, char* output );
-
-int getPaths( char** path );
-
-int validatePath( char* input );
-
+int exists( char* file );
+int getMode( char* mode );
+int getPaths( char** paths );
 void initialize();
-
+int validateMode( char* input, char* output );
+int validatePath( char* input );
 
 int main(){
 
     initialize();
 
     char mode;
-    getMode( &mode );
-
+    int notDone;
     char* paths[2];
+
+    do{
+
+        notDone = getMode( &mode );
+
+    } while( notDone );
+
     getPaths( paths );
 
     if( 0 == strcmp( &mode, "s" )){
@@ -46,39 +47,68 @@ int main(){
     return 0;
 }
 
+int exists( char* file ){
+
+    if( 0 == access( file, F_OK )){
+
+        return 1;
+    }
+
+    return 0;
+}
+
 int getMode( char* mode ){
     //On success, returns 0 and stores mode in char* mode
     //On fail returns 1 and the mode contents are not defined
 
-    char input[2];
+    char input[3];
 
-    if( NULL == fgets( input, 2, stdin )){
+    printf( "Please enter file mode [D/S]: " );
+    if( NULL == fgets( input, sizeof(input), stdin )){
 
         perror( "Error Invalid Input:  Please Enter a single character" );
         return 0;
     }
 
-    fgets( input, 2, stdin );
-
     if( 1 == validateMode( input, mode )){
 
-        printf( "mode succesfully validated.  Mode is: %s", mode );
+        perror( "Error Invalid Input" );
         return 1;
     }
 
-    validateMode( input, mode );
+    printf( "mode successfully validated.  Mode is: %s\n", mode );
     return 0;
 }
 
 void initialize(){
 
-    //make folders if not exist
-    //make files if not exist
-    //print out file/folder options
+    char* directories[2] = {"dir1", "dir2"};
+    char* filePaths[4] = {"dir1/a.txt", "dir1/b.txt", "dir2/c.txt", "dir2/d.txt"};
+
+    for( int i = 0; i < sizeof( directories ) / sizeof( directories[ 0 ] ); i++ ){
+
+        // We can ignore errors here, since an error (probably) just tells the the dir already exists, which is fine.
+        mkdir( directories[ i ], ACCESSPERMS);
+    }
+
+    printf( "Current directories and contents that are available for modification:\n" );
+
+    for( int i = 0; i < sizeof( filePaths ) / sizeof( filePaths[ 0 ] ); i++ ){
+
+        printf( "%s\n", filePaths[ i ] );
+
+        if( exists( filePaths[ i ] )){
+            continue;
+        }
+
+        // We call fopen to create the file, then immediately close the handle because we don't actually want it open.
+        FILE* file = fopen( filePaths[ i ], "w+" );
+        fclose( file );
+    }
+
 }
 
 int validateMode( char* input, char* output ){
-
 
     char mode = (char) tolower( input[ 0 ] );
 
@@ -102,43 +132,48 @@ int validatePath( char* input ){
     int sentinel = 0;
     char* pathComponents[2];
 
-    while( token != NULL ){
-        strsep(&token, "/");
+    while( token != NULL){
+        strsep( &token, "/" );
 
         // We explicitly only need to care about one step deeper into the file hierarchy
         // so I don't have to malloc this.
-        if ( 2 > sentinel ){
-            pathComponents[sentinel] = token;
+        if( 2 > sentinel ){
+            pathComponents[ sentinel ] = token;
             sentinel++;
+
         } else {
+
             // This means strsep is not yet null, yet we've found another token past the first
             // two,  this is invalid
 
-            perror("Invalid input:  Malformed path");
+            perror( "Invalid input:  Malformed path" );
             return 1;
         }
     }
+
+    // Now we check to ensure that we found at least one "/" (i.e. at least two tokens)
 
     return 0;
 }
 
 int getPaths( char** paths ){
+    // On success, returns 0 and stores both file paths in *paths.
+    // On fail returns 1 and the contents of **paths is not defined
 
     char input[1024];
     int sentinel = 0;
 
     while( 2 > sentinel ){
 
+        printf( "Enter File path %i: ", sentinel + 1);
         if( NULL == fgets( input, sizeof( input ), stdin )){
-            perror( "Error Invalid Input:  Please enter a valid path" );
+            perror( "Please enter a valid path (dir1/someFile.ext)" );
             continue;
         }
 
-        fgets( input, sizeof( input ), stdin );
-
         if( 1 == validatePath( input )){
 
-            perror( "Error Invalid Input:  Please enter a valid path" );
+            perror( "Please enter a valid path (dir1/someFile.ext)" );
             continue;
         }
 
